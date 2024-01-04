4e38a0b1e61cfad46d926f7f41a0c622f5faa8ab
tntn245 <tranngoctonhu2405@gmail.com>
2024-01-05 01:38:19 +0700

    postgresql & insert

---
+++ myapp/models.py
@@ -0,0 +1,6 @@
+from .extension import db 
+
+class User(db.Model):
+    # __tablename__='user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
---
+++ myapp/routes.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, Flask, jsonify, request
+import numpy as np
+import cv2
+from ultralytics import YOLO
+from PIL import Image
+from collections import Counter
+import matplotlib.pyplot as plt
+import io
+import os
+import base64
+from .extension import db
+from .models import User
+
+app = Blueprint('app', __name__)
+
+@app.route('/', methods = ['GET'])
+def get_articles():
+    return jsonify({"Hello":"World"})
+
+@app.route('/test', methods=['GET'])
+def test_connection():
+    return jsonify({"message": "Connection successful!"})
+
+@app.route('/add', methods=['GET'])
+def add_user():
+    db.session.add(User(id=2,username="username"))
+    db.session.commit()
+    return jsonify({"message": "Connection successful!"})
+
+@app.route('/detect', methods=['POST'])
+def detect_objects():
+    try:
+        # Load model
+        model = YOLO("best.pt")
+
+        # Print keys and information about received files for debugging
+        print(request.files.keys())
+        print(request.files['image'])
+
+        # Get the image file from the request
+        file = request.files['image']
+
+        # Convert the file to a NumPy array
+        file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
+
+        # Decode the image
+        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
+        # Perform object detection
+        results = model.predict(image)
+
+        # Process the results and create a list of object counts
+        object_counts = []
+        for result in results:
+            names = result.names
+            counts = Counter(result.boxes.cls.tolist())
+
+            for class_id, count in counts.items():
+                object_counts.append({
+                    "class": names[class_id],
+                    "count": count
+                })
+
+        # Optional: Save the annotated image
+        im_array = results[0].plot()  # Sử dụng results[0] thay vì result
+        im = Image.fromarray(im_array[..., ::-1]) 
+        buffered = io.BytesIO()
+        im.save(buffered, format="PNG")  # Lưu hình ảnh vào buffer dưới định dạng PNG
+        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')  # Chuyển đổi thành Base64
+        
+        # im.save('annotated_image.jpg')
+
+        # Define the path or URL to the saved image
+        # saved_image_path = 'received_image.jpg'  # Update this with the actual path or URL
+
+        # Return a JSON response
+        response_data = {
+            'object_counts': object_counts,
+            'image': img_str
+            # 'saved_image_path': saved_image_path
+        }
+        return jsonify(response_data), 200
+
+    except Exception as e:
+        # Handle any exceptions that might occur during the processing
+        print(f"Error: {e}")
+        return jsonify({'error': 'An error occurred during processing.'}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
